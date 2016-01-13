7ecff0f85826908025d2da6aaabd5443f6b2e7be
arealg <anavm4_@hotmail.com>
2016-01-13 10:13:01 +0100

    Numerosos errores capturados

--- uaserver.py
+++ uaserver.py
@@ -7,6 +7,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 import os
+import time
 import xml.etree.ElementTree as ET
 
 
@@ -15,26 +16,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    def date_time(self, linea, opcion, IP, PORT):
+        fichero = list['log']['path']
+        outfile = open(fichero, 'a')
+        fecha = time.strftime('%Y%m%d%H%M%S' , time.gmtime())
+        linea = linea.replace('\r\n\r\n', ' ')
+        linea = linea.replace('\r\n', ' ')
+        if opcion == 'send':
+            linea = 'Send to ' + IP + ':' + str(PORT) + ': ' + linea
+        elif opcion == 'receive':
+            linea = 'Received from ' + IP + ':' + str(PORT) + ': ' + linea
+        fecha = fecha + ' ' + linea + '\n'
+        outfile.write(fecha)
+        outfile.close()
 
     dicc = {}
     def handle(self):
+        IP = self.client_address[0]
+        PUERTO = self.client_address[1]
         while 1:
             line = self.rfile.read()
             if not line:
                 break
             linea = line.decode('utf-8')
             lista = linea.split()
-            if lista[0] == 'INVITE' or lista[0] == 'ACK' or lista[0] == 'BYE':
-                pass
-            else:
-                self.wfile.write(b'SIP/2.0 405 Method Not Allowed')
-                break
-
-            if (not 'sip' in lista[1].split(':') or
-                  not 'SIP/2.0' in lista or not '@' in lista[1]):
-                self.wfile.write(b'SIP/2.0 400 Bad Request')
-                break
 
             if 'Content-Type:' in lista:
                 info_user = {}
@@ -43,6 +48,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                 self.dicc[lista[1].split(':')[1]] = info_user
 
             if 'INVITE' in lista:
+                self.date_time(linea, 'receive', IP, PUERTO)
                 msn = ('Content-Type: application/sdp' + '\r\n\r\n' + 'v=0' + '\r\n'
                       + 'o=' + list['account']['username'] + ' ' + list['uaserver']['ip'] + '\r\n'
                       + 's=misesion' + '\r\n' + 't=0' + '\r\n' + 'm=audio '
@@ -52,18 +58,21 @@ class EchoHandler(socketserver.DatagramRequestHandler):
                       + '\r\n\r\n' + 'SIP/2.0 200 OK' + '\r\n' + msn
                       + '\r\n\r\n')
                 self.wfile.write(bytes(LINE, 'utf-8'))
+                self.date_time(LINE, 'send', IP, PUERTO)
+
 
             elif 'ACK' in lista:
-                print(self.dicc)
+                self.date_time(linea, 'receive', IP, PUERTO)
                 login = lista[1].split(':')[1]
-                print('\r\n\r\n' + linea + '\r\n\r\n')
                 rtp_msn = './mp32rtp -i ' + self.dicc[login]['ip'] + ' -p ' + self.dicc[login]['puerto']
                 rtp_msn = rtp_msn + ' < ' + list['audio']['path']
                 os.system(rtp_msn)
 
             elif 'BYE' in lista:
-                self.wfile.write(b'SIP/2.0 200 OK'+ b'\r\n\r\n')
-
+                self.date_time(linea, 'receive', IP, PUERTO)
+                LINE = 'SIP/2.0 200 OK'+ '\r\n\r\n'
+                self.wfile.write(bytes(LINE, 'utf-8'))
+                self.date_time(LINE, 'send', IP, PUERTO)
 
 if __name__ == "__main__":
 
